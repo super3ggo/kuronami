326d7e3104eba731e1763bfa2474f13d87c09ad0
inj0h <14225024+inj0h@users.noreply.github.com>
2024-08-31 20:47:00 -0500

    Stage 08-G:
    Add face customizations for only vanilla packages going forward.
    
    - Update TODOs
    - Update colors:
      - Remove:
        - blue   = #8ca5c6
        - greens = #668b8b, #a8d172
        - red    = #b6a6bf
        - yellow = #b89970
     - Add:
       - orange = #e9ad7f
       - violet = #a9acc8
    - Remove faces:
      - corfu
      - error
      - font-lock-warning-face

--- kuronami-theme.el
+++ kuronami-theme.el
@@ -45,68 +45,62 @@
 
 ;;; Code:
 
+;; TODO() Update this description.
 (deftheme kuronami
   "Cool autumnal colors against a deep blue background.")
 
-;; TODO() Add a color for a docstring... possibly a new one
-;; TODO() Edit yellow0 to make it less mustardy
-;; TODO() Edit blue2 or try replacing / removing it... See how it updates after updating yellow0
-(let ((kuronami-black0  "#181a26")  ; Emacs default theme Deeper Blue
+;; TODO() Update blue2 so that it pops more next to violet0.
+;; TODO() Find some other use for green0?
+;; TODO() Add note about only providing colors for vanilla packages.
+(let ((kuronami-black0  "#181a26")  ; Vanilla Emacs theme Deeper Blue default background color
       (kuronami-black1  "#464751")  ; black0 -> 2 tints lighter
       (kuronami-blue0   "#2e41ac")  ; Random image of blue Unit-00
       (kuronami-blue1   "#7fbbe9")  ; Official Ayanami Blue!
-      (kuronami-blue2   "#8ca5c6")  ; yellow0 -> complementary #708fb8 -> 2 tints lighter
-      (kuronami-green0  "#668b8b")  ; Emacs default "pale turquoise 4"
-      (kuronami-green1  "#65bab4")  ; blue1 -> analogous #7fe9e2 -> 2 shades darker
-      (kuronami-green2  "#a8d172")  ; blue1 -> triadic -> 1 shade darker
-      (kuronami-red0    "#e97f86")  ; blue1 -> triadic #e97fbb -> analogous
-      (kuronami-red1    "#b6a6bf")  ; Ayanami Blue -> modified using color wheel
-      (kuronami-yellow0 "#b89970")) ; Based on some Emacs default burlywood3 / #cdaa7d but a shade darker
+      (kuronami-blue2   "#acc7db")  ; blue1 -> monochromatic lighter (website does not produce the same hex code)
+      (kuronami-green0  "#65bab4")  ; blue1 -> analogous #7fe9e2 -> 2 shades darker
+      (kuronami-orange0 "#e9ad7f")  ; blue1 -> triadic
+      (kuronami-red0    "#e97f86")  ; orange0 -> analogous
+      (kuronami-violet0 "#a9acc8")) ; black0 -> monochromatic lighter (website does not produce the same hex code)
 
   (custom-theme-set-faces
    `kuronami
 
-   ;;; Vanilla:
-
    ;; UI:
    `(cursor              ((t (:background ,kuronami-red0))))
-   `(default             ((t (:background ,kuronami-black0 :foreground ,kuronami-red1))))
-   `(error               ((t (:foreground ,kuronami-red0 :weight bold))))
+   `(default             ((t (:background ,kuronami-black0 :foreground ,kuronami-violet0))))
    `(fringe              ((t (:background ,kuronami-black0))))
-   `(highlight           ((t (:background ,kuronami-green1))))
+   `(highlight           ((t (:background ,kuronami-blue0))))
    `(isearch             ((t (:background ,kuronami-red0 :foreground ,kuronami-black0))))
-   `(lazy-highlight      ((t (:background ,kuronami-green0))))
    `(line-number         ((t (:inherit default :foreground ,kuronami-blue2))))
    `(link                ((t (:foreground ,kuronami-blue1 :italic t :underline t))))
-   `(link-visited        ((t (:foreground ,kuronami-red1 :italic t :underline t))))
+   `(link-visited        ((t (:foreground ,kuronami-violet0 :italic t :underline t))))
    `(match               ((t (:inherit lazy-highlight))))
    `(minibuffer-prompt   ((t (:foreground ,kuronami-blue1))))
    `(region              ((t (:extend nil :background ,kuronami-blue0)))) ; Like Vim!
-   `(show-paren-match    ((t (:background ,kuronami-blue2))))
+   `(show-paren-match    ((t (:background ,kuronami-blue1))))
    `(show-paren-mismatch ((t (:background ,kuronami-red0))))
 
    `(font-lock-builtin-face           ((t (:inherit default))))
    `(font-lock-comment-face           ((t (:foreground ,kuronami-blue1 :italic t))))
    `(font-lock-comment-delimiter-face ((t (:inherit font-lock-comment-face))))
    `(font-lock-constant-face          ((t (:inherit default))))
-   `(font-lock-doc-face               ((t (:foreground ,kuronami-green2))))
+   `(font-lock-doc-face               ((t (:inherit font-lock-comment-face))))
    `(font-lock-function-name-face     ((t (:inherit default))))
-   `(font-lock-keyword-face           ((t (:inherit font-lock-builtin-face))))
-   `(font-lock-negation-char-face     ((t (:foreground ,kuronami-red0))))
-   `(font-lock-preprocessor-face      ((t (:inherit font-lock-builtin-face))))
-   `(font-lock-string-face            ((t (:foreground ,kuronami-green1))))
-   `(font-lock-type-face              ((t (:inherit font-lock-builtin-face))))
+   `(font-lock-keyword-face           ((t (:inherit default))))
+   `(font-lock-negation-char-face     ((t (:inherit default))))
+   `(font-lock-preprocessor-face      ((t (:inherit default))))
+   `(font-lock-string-face            ((t (:foreground ,kuronami-green0))))
+   `(font-lock-type-face              ((t (:inherit default))))
    `(font-lock-variable-name-face     ((t (:inherit default))))
-   `(font-lock-warning-face           ((t (:foreground ,kuronami-red0 :bold t :italic t))))
 
    ;; Listed alphabetically from this point.
 
-   `(compilation-column-number  ((t (:foreground ,kuronami-red1))))
+   `(compilation-column-number  ((t (:foreground ,kuronami-blue2))))
    `(compilation-line-number    ((t (:foreground ,kuronami-blue2))))
    `(compilation-mode-line-exit ((t (:inherit compilation-info))))
    `(compilation-mode-line-fail ((t (:inherit compilation-error))))
 
-   `(completions-common-part      ((t (:foreground ,kuronami-red1))))
+   `(completions-common-part      ((t (:foreground ,kuronami-violet0))))
    `(completions-first-difference ((t (:foreground ,kuronami-red0 :bold t))))
 
    `(dired-directory ((t (:foreground ,kuronami-blue2))))
@@ -115,14 +109,14 @@
    `(flyspell-duplicate ((t nil))) ; Setting "flyspell-duplicate-distance" does not work for Emacs 27.2 on macOS x86 so disable the Face.
    `(flyspell-incorrect ((t (:underline (:color ,kuronami-red0 :style wave)))))
 
-   `(ido-first-match ((t (:foreground ,kuronami-blue1 :italic t))))
-   `(ido-only-match  ((t (:foreground ,kuronami-green2 :bold t :italic t))))
-   `(ido-subdir      ((t (:foreground ,kuronami-blue2)))) ;; TODO() Update?
+   `(ido-first-match ((t (:foreground ,kuronami-orange0))))
+   `(ido-only-match  ((t (:foreground ,kuronami-red0 :bold t))))
+   `(ido-subdir      ((t (:foreground ,kuronami-blue2))))
 
-   `(mode-line           ((t (:background ,kuronami-red1 :foreground ,kuronami-black0)))) ; Just colors. No "boxing" effect.
+   `(mode-line           ((t (:background ,kuronami-violet0 :foreground ,kuronami-black0)))) ; Just colors. No "boxing" effect.
    `(mode-line-buffer-id ((t nil)))
    `(mode-line-emphasis  ((t nil)))
-   `(mode-line-inactive  ((t (:background ,kuronami-black1 :foreground ,kuronami-red1))))
+   `(mode-line-inactive  ((t (:background ,kuronami-black1 :foreground ,kuronami-violet0))))
 
    ;; This theme assumes the user has whitespace-style configured for the
    ;; following Faces:
@@ -153,22 +147,16 @@
    `(whitespace-hspace                 ((t (:background ,kuronami-black0 :foreground ,kuronami-black1))))
    `(whitespace-indentation            ((t (:inherit whitespace-hspace))))
    `(whitespace-line                   ((t (:inherit whitespace-hspace))))
-   `(whitespace-missing-newline-at-eof ((t (:background ,kuronami-yellow0 :foreground ,kuronami-black1))))
+   `(whitespace-missing-newline-at-eof ((t (:background ,kuronami-orange0 :foreground ,kuronami-black1))))
    `(whitespace-newline                ((t (:inherit whitespace-hspace))))
    `(whitespace-space                  ((t (:inherit whitespace-hspace))))
    `(whitespace-space-after-tab        ((t (:inherit whitespace-hspace))))
    `(whitespace-space-before-tab       ((t (:inherit whitespace-hspace))))
    `(whitespace-tab                    ((t (:inherit whitespace-hspace))))
-   `(whitespace-trailing               ((t (:inherit whitespace-missing-newline-at-eof))))
-
-   ;;; Third Party:
-
-   `(corfu-current ((t (:background ,kuronami-green0)))))
+   `(whitespace-trailing               ((t (:inherit whitespace-missing-newline-at-eof)))))
 
   (custom-theme-set-variables
    'kuronami
-
-   ;;; Vanilla:
 
    ;; TODO() Determine a more elegant way to do this?
    ;; TODO() Try adding just the color you want to change instead of referring
